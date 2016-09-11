5090a72f800de4b21cefc9e9636f8de298936cf5
frizar <vkfrizar@gmail.com>
2016-09-11 11:48:33 +0300

    redesign

--- frontend/js/newGame.js
+++ frontend/js/newGame.js
@@ -1,19 +1,12 @@
 'use strict';
 
 const BaseComponent = require('./baseComponent');
-const compiledTemplate = require('../templates/new-game.hbs');
 
 class NewGame extends BaseComponent {
     constructor(options) {
         super(options.element);
 
-        this.on('click', this._onClick.bind(this), '[data-element="new-game-button"]');
-
-        this.render();
-    }
-
-    render() {
-        this._el.innerHTML = compiledTemplate({});
+        this.on('click', this._onClick.bind(this));
     }
 
     _onClick(e) {
